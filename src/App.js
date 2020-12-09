6498fb5be6b2d03a92151f91a5c40c27ebba2ef5
Katie Zemon <katiezemon@gmail.com>
2020-12-09 15:09:34 -0600

    first page wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import AppBar from './AppBar'
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import BasicTable from './MainPageTable'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppBar/>
+            <Button variant="contained" color="primary">
+                Create Review
+            </Button>
+            <Container maxWidth="md">
+
+
+                Leaders
+                <BasicTable/>
+
+                More
+            </Container>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/MainPageTable.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+
+const rows = [
+    {name:'Paul Pennel', points:50},
+    {name:'Alex Lindemann', points:50},
+    {name:'Shushmita Maganty', points:49},
+    {name:'Katie Zemon', points:47},
+    {name:'Kyle Thompson', points:47},
+];
+
+export default function BasicTable() {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Points</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.points}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
